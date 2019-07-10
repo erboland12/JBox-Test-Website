0eedcf4df8571c4cfdac77fe413eef5b9cbf4130
Eric Boland <bolane1@sigi.us.selective.com>
2019-07-10 10:36:34 -0400

    Updated global footer

---
+++ Website.Start/Pages/Gallery.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace Website.Start.Pages
+{
+    public class GalleryModel: PageModel
+    {
+        public string Message;
+
+        public void OnGet()
+        {
+            Message = "Authentic Documentation";
+        }
+    }
+}
